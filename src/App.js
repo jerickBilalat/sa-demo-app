a91e59df4eca80fecee0ba35f093c5c19b15a26a
Jerick Bilalat <jerickbilalat@jrk-air.local>
2020-11-23 22:23:55 -0600

    created a http client facade  and a useAsync hook for fetching api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {clientFacade as client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
 
 function App() {
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
+
+  React.useEffect(() => {
+    run(client('spending/by?payPeriodId=5fbc4971c223440b10607920&type=normal'))
+  }, [run])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && <h1>Loading...</h1>}
+      {isError && (
+        <h2>
+          <h2>Error:</h2>
+          {error}
+        </h2>
+      )}
+      {isSuccess && console.log(data)}
+    </>
   )
 }
 
--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,5 +1,29 @@
-function clientFacade( endPoint, customConfig = {}) {
-    
+import axios from 'axios'
+
+function clientFacade(endPoint, customConfig = {}) {
+  const config = {
+    method: 'GET',
+    url: `${process.env.REACT_APP_API_URL}/${endPoint}`,
+    headers: {
+      'x-auth-token':
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZmJjNDkzZWMyMjM0NDBiMTA2MDc5MWYiLCJpYXQiOjE2MDYxNzUwMzgsInVzZXJuYW1lIjoiTWVsLWpyayIsImlkIjoiNWZiYzQ5M2VjMjIzNDQwYjEwNjA3OTFmIn0.FT-uiUkoCkGGI66SM-Pek8Dg0QP9wGyq1OoixuWpQ30',
+    },
+    ...customConfig,
+  }
+
+  return axios(config).then(handleResponse, handleError)
 }
 
-export { clientFacade };
+function handleResponse(res) {
+  if (res.statusText === 'OK') {
+    return res.data
+  } else {
+    return Promise.reject(res)
+  }
+}
+
+function handleError(err) {
+  return Promise.reject(err.response.data.message)
+}
+
+export {clientFacade}
