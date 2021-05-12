a1ae40e9aedd758d94231a191b4576d7e9d83c4b
Jerick Bilalat <jrk.email@yahoo.com>
2021-05-12 07:49:18 -0500

    refactor reducer addSpending, createSpending, and deleteSpending

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -34,6 +34,100 @@ const modifyPeriod = (state, action) => {
   }
 }
 
+const createSpending = (state, action) => {
+  const {
+    pay,
+    remainingBudget,
+    continuedFixedSpendings,
+    continuedGoals,
+  } = action.payload
+
+  const currentUpdatedPeriod = {
+    ...state.payPeriods[state.payPeriods.length - 1],
+    remainingBudget,
+  }
+
+  const newPayPeriod = {
+    _id: uuidv4(),
+    pay,
+    remainingBudget: '0.00',
+    refUser: state.userID,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+
+  const oldPeriods = state.payPeriods.filter(
+    x => x._id !== currentUpdatedPeriod._id,
+  )
+
+  const carryOverFixedSpendings = [
+    ...state.currentSpendings
+      .filter(spending => {
+        const spendingId = spending._id
+        if (continuedFixedSpendings.includes(spendingId)) {
+          return true
+        } else {
+          return false
+        }
+      })
+      .map(spending => {
+        return {
+          ...spending,
+          refPayPeriods: [...spending.refPayPeriods, newPayPeriod._id],
+        }
+      }),
+  ]
+  const carryOverGoalSpendings = [
+    ...state.currentSpendings
+      .filter(spending => {
+        const spendingId = spending._id
+        if (continuedGoals.includes(spendingId)) {
+          return true
+        } else {
+          return false
+        }
+      })
+      .map(spending => {
+        return {
+          ...spending,
+          refPayPeriods: [...spending.refPayPeriods, newPayPeriod._id],
+          goalBalance: currency(spending.amount).add(spending.goalBalance),
+        }
+      }),
+  ]
+  return {
+    ...state,
+    emrRemainingBalance: currency(state.emrRemainingBalance)
+      .add(state.emrCommitmentAmount)
+      .format(),
+    payPeriods: [...oldPeriods, currentUpdatedPeriod, newPayPeriod],
+    currentSpendings: [...carryOverFixedSpendings, ...carryOverGoalSpendings],
+  }
+}
+
+const addSpending = (state, action) => {
+  let aSpending = action.payload
+  let {currentSpendings} = state
+
+  if (currentSpendings.map(spending => spending._id).includes(aSpending._id)) {
+    currentSpendings = currentSpendings.filter(x => x._id !== aSpending._id)
+  }
+
+  return {
+    ...state,
+    currentSpendings: [...currentSpendings, aSpending],
+  }
+}
+
+const deleteSpending = (state, action) => {
+  return {
+    ...state,
+    currentSpendings: [
+      ...state.currentSpendings.filter(x => x._id !== action.payload._id),
+    ],
+  }
+}
+
 export default function userDataReducer(state, action) {
   switch (action.type) {
     case actions.UPDATE_USER_SETTINGS:
@@ -41,99 +135,11 @@ export default function userDataReducer(state, action) {
     case actions.MODIFY_PERIOD:
       return modifyPeriod(state, action)
     case actions.CREATE_PERIOD:
-      const {
-        pay,
-        remainingBudget,
-        continuedFixedSpendings,
-        continuedGoals,
-      } = action.payload
-
-      const currentUpdatedPeriod = {
-        ...state.payPeriods[state.payPeriods.length - 1],
-        remainingBudget,
-      }
-
-      const newPayPeriod = {
-        _id: uuidv4(),
-        pay,
-        remainingBudget: '0.00',
-        refUser: state.userID,
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-      }
-
-      const oldPeriods = state.payPeriods.filter(
-        x => x._id !== currentUpdatedPeriod._id,
-      )
-
-      const carryOverFixedSpendings = [
-        ...state.currentSpendings
-          .filter(spending => {
-            const spendingId = spending._id
-            if (continuedFixedSpendings.includes(spendingId)) {
-              return true
-            } else {
-              return false
-            }
-          })
-          .map(spending => {
-            return {
-              ...spending,
-              refPayPeriods: [...spending.refPayPeriods, newPayPeriod._id],
-            }
-          }),
-      ]
-      const carryOverGoalSpendings = [
-        ...state.currentSpendings
-          .filter(spending => {
-            const spendingId = spending._id
-            if (continuedGoals.includes(spendingId)) {
-              return true
-            } else {
-              return false
-            }
-          })
-          .map(spending => {
-            return {
-              ...spending,
-              refPayPeriods: [...spending.refPayPeriods, newPayPeriod._id],
-              goalBalance: currency(spending.amount).add(spending.goalBalance),
-            }
-          }),
-      ]
-      return {
-        ...state,
-        emrRemainingBalance: currency(state.emrRemainingBalance)
-          .add(state.emrCommitmentAmount)
-          .format(),
-        payPeriods: [...oldPeriods, currentUpdatedPeriod, newPayPeriod],
-        currentSpendings: [
-          ...carryOverFixedSpendings,
-          ...carryOverGoalSpendings,
-        ],
-      }
+      return createSpending(state, action)
     case actions.ADD_SPENDING:
-      let spending = action.payload
-      let currentSpendings = state.currentSpendings
-
-      if (currentSpendings.map(x => x._id).includes(spending._id)) {
-        currentSpendings = state.currentSpendings.filter(
-          x => x._id !== spending._id,
-        )
-      }
-
-      return {
-        ...state,
-        currentSpendings: [...currentSpendings, spending],
-      }
+      return addSpending(state, action)
     case actions.DELETE_SPENDING:
-      const deletedSpendingId = action.payload._id
-      return {
-        ...state,
-        currentSpendings: [
-          ...state.currentSpendings.filter(x => x._id !== deletedSpendingId),
-        ],
-      }
+      return deleteSpending(state, action)
     default:
       return state
   }
